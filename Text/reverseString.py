fdf11d56a08831fe76f52f304e95b6d3692b8735
Kam Fataliyev <k.fataliyev@gmail.com>
2017-06-10 00:45:02 +1000

    Added reverseString

---
+++ Text/reverseString.py
@@ -0,0 +1,8 @@
+"""
+Reverse a String
+Enter a string and the program will reverse it and print it out.
+"""
+
+text = raw_input('Enter a string \n>>> ')
+reverseText = text[::-1]
+print reverseText
