74c56a605c0d8e7f3bedaaf379d58952c8daa71d
Kam Fataliyev <k.fataliyev@gmail.com>
2017-07-12 03:07:54 +1000

    Added VigenereCipher

--- Text/ciphers.py
+++ Text/ciphers.py
@@ -3,14 +3,29 @@
 import sys
 
 def ceasarCipher(text, shift):
-	encodedText = ''.join([chr(97+(((ord(i)+shift)%97)%26)) if i.islower() else chr(65+(((ord(i)+shift)%65)%26)) if i.islower() else i for i in text])
+#	(ord(i)%65+shift)%26 + 65
+	encodedText = ''.join([chr((ord(i)%65+shift)%26 + 65) if i.isalpha() else i for i in text])
+	return encodedText
+
+
+def vigenereCipher(text, keyword):
+	encodedText = ''
+	n = 0
+	for i in range(len(text)):
+		if text[i].isalpha():
+			shift = ord(keyword[n%len(keyword)]) % 65
+			letter = ceasarCipher(text[i], shift)
+			n += 1
+		else:
+			letter = text[i]
+		encodedText += letter
 	return encodedText
 
 
 
 
-
 try:
+	print 'All letters will be capitalized.'
 	print 'Please choose a cipher method:'
 	print '\t1 - Ceasar\n\t2 - Vigenere\n\t3 - Vernam'
 	choice = int(raw_input('>>> '))
@@ -22,12 +37,17 @@ try:
 			lines.append(line)
 		else:
 			break
-	text = '\n'.join(lines)
+	text = ('\n'.join(lines)).upper()
 	print 'Your text is: \n%s' % text
 	if choice == 1:
 		shift = int(raw_input('Please enter a number to shift. Add a negative number to shift to the left: '))
 		encodedText = ceasarCipher(text, shift)
 		print encodedText
+	elif choice == 2:
+		keyword = raw_input('Please enter a keyword: ').upper()
+		print 'your keyword is %s' %keyword
+                encodedText = vigenereCipher(text, keyword)
+                print encodedText
 
 except:
 	sys.exit('Invalid input')
