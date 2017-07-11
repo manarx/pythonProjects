f48f97242445656a7e57bf4ae881e6dd773962bc
Kam Fataliyev <k.fataliyev@gmail.com>
2017-07-11 20:02:14 +1000

    Modified fibonacchi to print the sequence up the entered number

--- Numbers/fibonacciSequence.py
+++ Numbers/fibonacciSequence.py
@@ -1,9 +1,14 @@
-def fibonacci(n):
+#Fibonacci Sequence
+#Enter a number and have the program generate the Fibonacci
+#sequence to that number or to the Nth number.
+
+def printFibonacci(n):
 	n1, n2 = 1, 1
 	i = 2
+	print n1, n2, 
 	for i in range(2, n):
 		n1, n2 = n2, n1 + n2
-	return n2
+		print n2,
 
 user_input = raw_input('Enter a number: ')
-print fibonacci(int(user_input))
+printFibonacci(int(user_input))
